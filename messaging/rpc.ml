dc57d301dd109ded0f31e9045373fedd68d75730
Matt Hoyt <mrhoyt4@gmail.com>
2020-10-02 23:16:05 -0500

    Started working on the rpc calls.

--- messaging/rpc.ml
+++ messaging/rpc.ml
@@ -1,3 +1,6 @@
+open Core
+open Lwt.Infix
+
 module type Request_processor = sig
     type encoding = string
     type header
@@ -6,33 +9,52 @@ module type Request_processor = sig
     val decode_header : encoding -> header option
 
     (* Used to handle the incoming of the message. *)
-    val handle_message : header -> encoding -> (encoding * encoding)
+    val handle_message : header -> encoding -> (encoding * encoding) Lwt.t
 end
 
 module Make_Request_processor(R: Request_processor) = struct
+
+    type state = 
+        | Idle
+        | Running
     
     type t = {
         host_id: Host_manager.host_id;
         service_id: Host_manager.service_id;
         host_manager: Host_manager.t;
-        pull_socket: [`Push] Zmq.Socket.t;
-        pull_socket_lwt: [`Push] Zmq_lwt.Socket.t;
+        bind_url: string;
+        context: Zmq.Context.t;
+        state: state ref;
     }
 
     (* Creates a new request processor. *)
-    let make ctx host_id service_id host_manager =
-        let socket = Zmq.Socket.create ctx Zmq.Socket.push in
-        let lwt_socket = Zmq_lwt.Socket.of_socket socket in
+    let make ctx bind_url host_id service_id host_manager =
         {
             host_id;
             service_id;
             host_manager;
-            pull_socket=socket;
-            pull_socket_lwt=lwt_socket;
+            bind_url;
+            context=ctx;
+            state=ref Idle;
         }
 
     (* Starts the process for listening on the socket. *)
-    let listen (_: t) =
-        ()
+    let listen (t: t) =
+        match !(t.state) with
+        | Idle ->
+            t.state := Running;
+            let socket = Zmq.Socket.create t.context Zmq.Socket.pull in
+            let socket_lwt = Zmq_lwt.Socket.of_socket socket in
+            let rec handler_loop () =
+                Zmq_lwt.Socket.recv socket_lwt
+                >>= fun msg -> (
+                    match (R.decode_header msg, Zmq.Socket.has_more socket) with
+                    | (Some header, true) -> Zmq_lwt.Socket.recv socket_lwt
+                        >>= fun msg -> R.handle_message header msg
+                        >>= fun _ -> Lwt.return_unit
+                    | _ -> Lwt.return_unit)
+                >>= fun _ -> handler_loop () in
+            handler_loop ()
+        | Running -> Lwt.return_unit
 
 end
--- messaging/rpc.mli
+++ messaging/rpc.mli
@@ -1,3 +1,5 @@
+open! Core
+
 module type Request_processor = sig
     type encoding = string
     type header
@@ -6,17 +8,24 @@ module type Request_processor = sig
     val decode_header : encoding -> header option
 
     (* Used to handle the incoming of the message. *)
-    val handle_message : header -> encoding -> (encoding * encoding)
+    val handle_message : header -> encoding -> (encoding * encoding) Lwt.t
 end
 
 module Make_Request_processor(R: Request_processor) : sig
     
     type t
 
-    (* Creates a new request processor. *)
-    val make : Zmq.Context.t ->  Host_manager.host_id -> Host_manager.service_id -> Host_manager.t -> t
+    (* Creates a new request processor. 
+     * Arguments:
+     * ctx: Zmq.Context.t - The zmq context
+     * bind_url - The binding url for the socket.
+     * host_id - The id for this host.
+     * service_id - The id for this service.
+     * host_manager - The host manager containing the location of all the services and hosts.
+     *)
+    val make : Zmq.Context.t -> string ->  Host_manager.host_id -> Host_manager.service_id -> Host_manager.t -> t
 
     (* Starts the process for listening on the socket. *)
-    val listen : t -> unit
+    val listen : t -> unit Lwt.t
 
 end
